36811510427d6a70171a56a5b0adb4430ce577e8
Luke Jones <nnblc565@gmail.com>
2021-02-24 12:03:43 +1030

    First push

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed = 1000;
+
+    [Range(0, 90)]
+    public float maxAngle = 70;
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            Vector3 angles = transform.eulerAngles;
+            float dx = -Input.GetAxis("Mouse Y");
+            float dy = Input.GetAxis("Mouse X");
+            float dxDir = dx < 0 ? -1 : 1;
+            // Debug.Log(dxDir);
+
+            dx = angles.x + dx * speed * Time.deltaTime;
+
+            //Clamp the angle within 70 degrees
+            if (dx < 360 && dx < 360 - maxAngle && dx > 180)
+                dx = 360 - maxAngle;
+            else if (dx >= 0 && dx > maxAngle && dx < 180)
+                dx = maxAngle;
+
+            angles.x = dx;
+
+            angles.y += dy * speed * Time.deltaTime;
+            transform.eulerAngles = angles;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Cursor.lockState = Cursor.lockState == CursorLockMode.None ? CursorLockMode.Locked : CursorLockMode.None;
+    }
+}
---
+++ Assets/Scripts/Player/CharacterMover.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+    public float speed = 10;
+    public float jumpForce = 10;
+
+    protected CharacterController cc = null;
+    protected Vector2 moveInput = new Vector2();
+    protected Vector3 velocity = new Vector2();
+
+    protected bool jumpInput = false;
+
+    void Awake()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveInput.x = Input.GetAxis("Horizontal");
+        moveInput.y = Input.GetAxis("Vertical");
+        jumpInput = jumpInput == false ? Input.GetButton("Jump") : true;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 delta = (moveInput.x * transform.right + moveInput.y * Vector3.Cross(transform.right, Vector3.up)) * speed;
+
+        if (cc.isGrounded || moveInput.x !=0 || moveInput.y != 0)
+        {
+            velocity.x = delta.x;
+            velocity.z = delta.z;
+        }
+
+        if (cc.isGrounded && velocity.y < 0)
+            velocity.y = 0;
+
+        if (jumpInput && cc.isGrounded)
+            velocity.y = jumpForce;
+        jumpInput = false;
+
+        velocity += Physics.gravity * Time.fixedDeltaTime;
+
+        cc.Move(velocity * Time.fixedDeltaTime);
+    }
+}
