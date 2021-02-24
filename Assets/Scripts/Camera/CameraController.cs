71e85632cb01e209623d492c89d9654428eb8093
Luke Jones <nnblc565@gmail.com>
2021-02-24 15:32:52 +1030

    Commented the two scripts in doxygen format and fixed some cap issues on the player

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -1,25 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Controls the rotation of the camera and player
+/// </summary>
 public class CameraController : MonoBehaviour
-{
+{   
+    /// <summary>
+    /// The sensitivity of rotation
+    /// </summary>
+    [Tooltip("The sensitivity of rotation")]
     public float speed = 1000;
+    /// <summary>
+    /// The transform of the players origin or the equivalent location of the character mover
+    /// </summary>
+    [Tooltip("The transform of the players origin or the equivalent location of the character mover")]
     public Transform parentTrans = null;
+    /// <summary>
+    /// The transform of the head of the model
+    /// </summary>
+    [Tooltip("The transform of the head of the model")]
     public Transform trueParent = null;
-    public Vector3 trueParentOffset = new Vector3();
-
+    /// <summary>
+    /// The vertical offset from the center of the head of the model
+    /// </summary>
+    [Tooltip("The vertical offset from the center of the head of the model")]
+    public float trueParentOffset = 0.1f;
+    /// <summary>
+    /// The maximum angle the player can look up and down
+    /// </summary>
     [Range(0, 90)]
     public float maxAngle = 70;
+    /// <summary>
+    /// Disables the players mouse cursor
+    /// </summary>
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
     }
-    // Update is called once per frame
+    /// <summary>
+    /// Updates the players rotation and cameras rotation
+    /// </summary>
     void Update()
     {
         if (trueParent != null)
-            transform.position = trueParent.position + trueParentOffset;
+            transform.position = trueParent.position + transform.up * trueParentOffset;
 
         if (Cursor.lockState == CursorLockMode.Locked)
         {
--- Assets/Scripts/Player/CharacterMover.cs
+++ Assets/Scripts/Player/CharacterMover.cs
@@ -6,23 +6,52 @@ using UnityEngine;
 /// </summary>
 public class CharacterMover : MonoBehaviour
 {
+    /// <summary>
+    /// The maximum speed of the player
+    /// </summary>
+    [Tooltip("The maximum speed of the player")]
     public float speed = 10;
+    /// <summary>
+    /// The vertical velocity applied to the player upon jumping
+    /// </summary>
+    [Tooltip("The vertical velocity applied to the player upon jumping")]
     public float jumpForce = 10;
-
-    protected CharacterController cc = null;
+    /// <summary>
+    /// Storages the direction of movement
+    /// </summary>
     protected Vector2 moveInput = new Vector2();
+    /// <summary>
+    /// Stores the velocity
+    /// </summary>
     protected Vector3 velocity = new Vector3();
+    /// <summary>
+    /// Stores the direction to the nearest collsison with the character controller
+    /// </summary>
     protected Vector3 hitDir = new Vector3();
-
+    /// <summary>
+    /// Determines if we should jump
+    /// </summary>
     protected bool jumpInput = false;
+    /// <summary>
+    /// Stores a reference to the animator
+    /// </summary>
     protected Animator animator = null;
-
+    /// <summary>
+    /// A reference to the character controller.
+    /// The character controller is used to me the player
+    /// </summary>
+    protected CharacterController cc = null;
+    /// <summary>
+    /// Gets references to other components
+    /// </summary>
     void Awake()
     {
         cc = GetComponent<CharacterController>();
         animator = transform.GetChild(0).GetComponent<Animator>();
     }
-
+    /// <summary>
+    /// Updates moveInput and jump input
+    /// </summary>
     void Update()
     {
         moveInput.x = Input.GetAxis("Horizontal");
@@ -31,7 +60,9 @@ public class CharacterMover : MonoBehaviour
 
         animator.SetBool("Crouch", Input.GetButton("Crouch"));
     }
-
+    /// <summary>
+    /// Moves the player and causes them to jump
+    /// </summary>
     void FixedUpdate()
     {
         bool isGrounded = cc.isGrounded;
@@ -72,7 +103,10 @@ public class CharacterMover : MonoBehaviour
 
         cc.Move(velocity * Time.fixedDeltaTime);
     }
-
+    /// <summary>
+    /// Get hitDir
+    /// </summary>
+    /// <param name="hit">The hit info from the collision</param>
     private void OnControllerColliderHit(ControllerColliderHit hit)
     {
         hitDir = hit.point - transform.position;
