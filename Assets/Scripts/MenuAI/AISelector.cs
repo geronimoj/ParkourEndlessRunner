08e37173c89cc7f03dfb0b27b3c0f20a4d128276
Luke Jones <nnblc565@gmail.com>
2021-03-14 13:15:26 +1030

    Began working on the menu for swapping model

---
+++ Assets/Scripts/MenuAI/AISelector.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+/// <summary>
+/// Used when the player is selecting their character model
+/// </summary>
+[RequireComponent(typeof(Animator))]
+public class AISelector : MonoBehaviour
+{
+    /// <summary>
+    /// A reference to the animator on this object
+    /// </summary>
+    private Animator _animator = null;
+    /// <summary>
+    /// The information about this model
+    /// </summary>
+    [Tooltip("The information about this model")]
+    [SerializeField]
+    private ModelInfo m_modelInfo;
+    /// <summary>
+    /// The information about this object
+    /// </summary>
+    public ModelInfo ModelInfo => m_modelInfo;
+    /// <summary>
+    /// The offset from the inital spawn location applied to this objects transform until the AI has loaded its animations. This is in global co-ordinates
+    /// </summary>
+    [Tooltip("The offset from the inital spawn location applied to this objects transform until the AI has loaded its animations. This is in global co-ordinates")]
+    [SerializeField]
+    private Vector3 _spawnOffset = new Vector3();
+    /// <summary>
+    /// Stores wether the teleport has accured
+    /// </summary>
+    private bool teleported = false;
+    /// <summary>
+    /// Gets references and sets the position of the NPC
+    /// </summary>
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        transform.position += _spawnOffset;
+    }
+    /// <summary>
+    /// Checks if the AI has entered the animation and teleports them
+    /// </summary>
+    void Update()
+    {
+        if (!teleported && _animator.GetBool("InAnimation"))
+        {
+            teleported = true;
+            transform.position -= _spawnOffset;
+        }
+    }
+}
---
+++ Assets/Scripts/SwapScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+/// <summary>
+/// Contains a function for swapping the scenes
+/// </summary>
+public class SwapScene : MonoBehaviour
+{
+    /// <summary>
+    /// Swaps the scene to the scene with the given name
+    /// </summary>
+    /// <param name="sceneName">The name of the scene to swap too</param>
+    public void Swap(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+    }
+    /// <summary>
+    /// Loads a scene without deleting the previous scene
+    /// </summary>
+    /// <param name="sceneName">The name of the scene to load</param>
+    public void Load(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+    }
+}
